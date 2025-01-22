55682f67db446c54d774fba80fff161bd92fca54
Travis Frazier <travisfrazier@Traviss-MBP.lan>
2025-01-21 20:35:04 -0800

    updates

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,13 +3,10 @@ export default defineNuxtConfig({
 	css: [  
     "swiper/css/bundle", 
 		"bootstrap/dist/css/bootstrap.css",
-
 		"@/assets/css/animate.min.css",
 		"@/assets/css/blog.css",
 		"@/assets/css/font-awesome-pro.css",
 		"@/assets/css/mobilemenu.css",
-		"@/assets/fonts/remixicon.css",
-
 		"@/assets/css/spacing.css",
 		"@/assets/css/style.css",
 		"@/assets/css/responsive.css",
